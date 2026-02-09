9cf79f319982c9daa21a3c9aaf752bcbf5bfd6c7
Julie Price <julieprice@gmail.com>
2026-02-09 13:50:35 -0500

    Add /cheatcodes redirect to /sidekick_insp1

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,15 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  async redirects() {
+    return [
+      {
+        source: '/cheatcodes',
+        destination: '/sidekick_insp1',
+        permanent: true,
+      },
+    ]
+  },
 };
 
 export default nextConfig;
